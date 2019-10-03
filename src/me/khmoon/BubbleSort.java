64527b0fbff85d71ecc98df23e1adf7883694eb2
kwanghyunMoon <mgh3326@naver.com>
2019-10-04 02:00:23 +0900

    update bubble sort

--- src/me/khmoon/BubbleSort.java
+++ src/me/khmoon/BubbleSort.java
@@ -3,13 +3,23 @@ package me.khmoon;
 public class BubbleSort {
 
   static int[] bubbleSort(int[] inputArr) {
-    return inputArr;
+    int[] outArr = inputArr.clone();
+    for (int i = 0; i < outArr.length; i++) {
+      for (int j = 0; j < outArr.length - 1 - i; j++) {
+        if (outArr[j] > outArr[j + 1]) {
+          int tempValue = outArr[j];
+          outArr[j] = outArr[j + 1];
+          outArr[j + 1] = tempValue;
+        }
+      }
+    }
+    return outArr;
   }
 
   public static void main(String[] args) {
     int[] data = {4, 54, 2, 8, 63, 7, 55, 56};
-    int[] outArr = bubbleSort(data);
-    for (int i : outArr) {
+    int[] outputArr = bubbleSort(data);
+    for (int i : outputArr) {
       System.out.print(i + ", ");
     }
     System.out.println();
