a083b52757554baeb9faf05e24adfb874b693b26
kwanghyunMoon <mgh3326@naver.com>
2019-10-04 02:09:22 +0900

    update select sort

---
+++ src/me/khmoon/SelectSort.java
@@ -0,0 +1,33 @@
+package me.khmoon;
+
+public class SelectSort {
+  public static void main(String[] args) {
+    int[] data = {4, 54, 2, 8, 63, 7, 55, 56};
+    int[] outputArr = selectSort(data);
+    for (int i : outputArr) {
+      System.out.print(i + ", ");
+    }
+    System.out.println();
+  }
+
+  private static int[] selectSort(int[] data) {
+    int[] outArr = data.clone();
+    for (int i = 0; i < outArr.length - 1; i++) {
+      int tempMinValue = outArr[i];
+      int tempMinIndex = i;
+      for (int j = i + 1; j < outArr.length; j++) {
+        if (tempMinValue > outArr[j]) {
+          tempMinValue = outArr[j];
+          tempMinIndex = j;
+        }
+      }
+      if (i != tempMinIndex) {
+        int tempValue = outArr[i];
+        outArr[i] = outArr[tempMinIndex];
+        outArr[tempMinIndex] = tempValue;
+      }
+
+    }
+    return outArr;
+  }
+}
